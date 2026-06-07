dc0052f14344a14ae68f4629dafe3a1673ce2c40
ashilagirl <ashilaprashi85@gmail.com>
2026-06-07 19:00:51 +0530

    Delete data/stock.js

--- data/stock.js
+++
@@ -1,6 +0,0 @@
-// data/stock.js - Initial stock quantities (grams/ml/units)
-export const initialStock = {
-    Mango: 3000, Orange: 3000, Pineapple: 5000, Papaya: 3000,
-    Watermelon: 5000, Avocado: 2000, Lemon: 2000, Sugar: 5000,
-    "Lime Powder": 500, "Fresh Milk": 5000, Ice: 10000, Pepper: 200, CupSet: 1000
-};
